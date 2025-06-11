fc5977b31d93ddf39c29c6da255b71481604ba43
mikeyounge <mikeyoung.e@gmail.com>
2025-06-11 12:54:14 -0600

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Start all services in /etc/services.d in the background
+find /etc/services.d -type f -name run -executable -exec {} \; &
+
+# Wait briefly to ensure services initialize
+sleep 5
+
+# Execute the original Pi-hole entrypoint/command
+exec "$@"
